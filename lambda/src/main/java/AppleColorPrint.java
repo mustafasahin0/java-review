7734356951c3f76faa7258f3bee0172ce9972756
msahin <msahin@switchfly.com>
2022-03-16 19:45:05 -0600

    Added Functional Interface and Behavioral Param Example

--- lambda/src/main/java/AppleColorPrint.java
+++ lambda/src/main/java/AppleColorPrint.java
@@ -1,7 +1,7 @@
-public class AppleColorPrint implements ApplePrettyPrint{
+public class AppleColorPrint implements ApplePrettyPrint {
     @Override
     public String print(Apple apple) {
-        if(apple.getWeight()<200) return "A Light " + apple.getColor().toString() +" apple";
-        else return "A Heavy "+ apple.getColor().toString()+" apple";
+      String characteristic = apple.getWeight() > 150 ? "Heavy" : "Light";
+      return "A " + characteristic + " " + apple.getColor() + " apple";
     }
 }
---
+++ lambda/src/main/java/functionalInterfaces/Examples.java
@@ -0,0 +1,46 @@
+package functionalInterfaces;
+
+import java.util.function.*;
+
+public class Examples {
+
+    public static void main(String[] args) {
+
+        //************************PREDICATE**********/
+//Anonymous function
+//        Predicate<Integer> lesserThan = new Predicate<Integer>() {
+//            @Override
+//            public boolean test(Integer integer) {
+//                return false;
+//            }
+//        };
+
+        //    lesserThan.test(50);
+
+        Predicate<Integer> lesserThan = (a) -> a < 10; // Implementation of test method that belongs to Predicate interface
+        Boolean result = lesserThan.test(50);
+        System.out.println(result);
+
+        //************************CONSUMER**********/
+
+        Consumer<Integer> display = i -> System.out.println(i);
+        display.accept(50);
+
+        //************************BICONSUMMER**********/
+
+        BiConsumer<Integer, Integer> addTwo = (x, y) -> System.out.println(x + y);
+        addTwo.accept(100, 200);
+
+        //************************FUNCTION**********/
+
+        Function<String, String> fun = s -> "Hello " + s;
+        String str =fun.apply("Mustafa");
+        System.out.println(str);
+
+        //************************SUPPLIER**********/
+        Supplier<Double> randomValue = () -> Math.random();
+        System.out.println(randomValue.get());
+
+    }
+}
+
