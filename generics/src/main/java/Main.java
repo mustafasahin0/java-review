dfc8df46cc95ee085b5e8728b3c009d96196665e
msahin <msahin@switchfly.com>
2022-03-21 10:29:56 -0600

    Added Generics Example

---
+++ generics/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+           var list =  new GenericList<Integer>();
+           list.add(1);
+           int number = list.get(0);
+    }
+}
---
+++ generics/src/main/java/UserList.java
@@ -0,0 +1,4 @@
+public class UserList {
+    private User[] items = new User[10];
+    private int count;
+}
